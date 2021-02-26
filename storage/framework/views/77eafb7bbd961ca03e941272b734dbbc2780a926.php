0155d2439173666038581fa3709eed2327221699
Mohamed Shaban Abd El-Tawab <40707838+mo7amedshaban@users.noreply.github.com>
2021-02-26 06:07:34 +0200

    Add files via upload

---
+++ storage/framework/views/77eafb7bbd961ca03e941272b734dbbc2780a926.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('content'); ?>
+
+  <div class="intro-container">
+    <global-home></global-home>
+  </div>
+
+        
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/Security/Projects Web/Laravel/Blog/resources/views/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/dda0470b22d15e666b7460f32e88746650b7c99a.php
@@ -0,0 +1,15 @@
+<footer class="py-5 bg-dark" style="margin-top:70px;">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+</footer>
+
+ <!--modals-->
+ <!--register-->
+ 
+
+
+ <!--login-->
+ 
+ <!--modals-->
+<?php /**PATH /Users/Security/Projects Web/Laravel/Blog/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
