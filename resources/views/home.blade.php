a2f03296f4ee8369c3960b6e23fbd55f777dace9
Mohamed Shaban Abd El-Tawab <40707838+mo7amedshaban@users.noreply.github.com>
2021-02-26 05:56:03 +0200

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('content')
+
+  <div class="intro-container">
+    <global-home></global-home>
+  </div>
+
+        {{-- <!-- Pagination -->
+        <nav aria-label="...">
+          <ul class="pagination float-right">
+            <li class="page-item disabled">
+              <span class="page-link">Previous</span>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item active">
+              <span class="page-link">
+                2
+                <span class="sr-only">(current)</span>
+              </span>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#">Next</a>
+            </li>
+          </ul>
+        </nav>
+
+      </div> --}}
+
+@endsection
