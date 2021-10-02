fcf7d9d4c4c189def2093aa4e441aa6e1be124c6
Mohamed Shaban <mohamed.2015.ms880@gmail.com>
2021-10-02 18:49:00 +0200

    deploy on heroku

---
+++ storage/framework/views/5a10e7d8cb3d54c7eb7c61432e5d6820e34cd424.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('content'); ?>
+
+  <div class="intro-container">
+    <global-home></global-home>
+  </div>
+
+        
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/mac/Desktop/GitHub/Blog-with-chat/resources/views/home.blade.php ENDPATH**/ ?>
